510fc484ed8f790596414be51852c1df66a0ac75
Chris <christopher.b.kyle@gmail.com>
2024-05-08 20:08:57 +1200

    form done next step implement framer motion in resize

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,6 +4,26 @@ import SendEmail from "./SendEmail";
 
 export default function Links({clicked}) {
 
+    const [formRender, setFormRender] = useState(                
+    <table className={`form-table ${clicked ? '' : 'hidden'}`}>
+        <tr className={`${clicked ? '' : 'hidden'}`}>
+            <td className='form-text' ><label className={`${clicked ? '' : 'hidden'}`}>Name: </label></td>
+            <td style={{width: '85%'}} ><input type="text" name="from_name" className='form-input' placeholder='Name' required/></td>
+        </tr>
+        <tr className={`${clicked ? '' : 'hidden'}`}>
+            <td className='form-text' ><label>Email: </label></td>
+            <td style={{width: '85%'}}><input type="email" name="user_email" className='form-input' placeholder='Email' required/></td>
+        </tr>
+        <tr className={`${clicked ? '' : 'hidden'}`}>
+            <td className='form-text' style={{verticalAlign: 'top'}}><label>Message: </label></td>
+            <td colSpan='3' style={{width: '85%', height: '160px'}}><textarea name="message" className='form-input' placeholder='Message' style={{height: '155px'}} required/></td>
+        </tr>
+        <tr className={`${clicked ? '' : 'hidden'}`}>
+            <td colSpan='4' style={{textAlign: 'center'}}><input type="submit" value="Send" className='form-button button'/></td>
+        </tr>
+    </table>
+    )
+
     const form = useRef();
 
     const sendEmail = (e) => {
@@ -11,12 +31,15 @@ export default function Links({clicked}) {
 
         emailjs.sendForm('service_kmcgxy9', 'template_74hhmjy', form.current, {
             publicKey: 'Rp8Mr9QaGldBamGXH',
+            // publicKey: 'TEST,'
         })
         .then(() => {
             console.log('Email Sent');
+            setFormRender(<div className='email-confirmation'>Thank you, email has been sent to christopher.b.kyle@gmail.com</div>)
         })
         .catch((err) => {
             console.log('Email not send', err.text);
+            // setFormRender(<div className='email-confirmation'>Thank you, email has been sent to christopher.b.kyle@gmail.com</div>)
         })
     }
 
@@ -25,24 +48,25 @@ export default function Links({clicked}) {
         <div className={`contact ${clicked ? '' : 'hidden'}`}>
             <h2 style={{margin: '10px'}}>Contact</h2>
             <form ref={form} onSubmit={sendEmail} className='email-form'>
-            <table className='form-table'>
-                <tr>
-                    <td className='form-text' style={{width: '15%'}}><label>Name: </label></td>
-                    <td style={{width: '85%'}}><input type="text" name="user_name" className='form-input border' placeholder='Name'/></td>
-                </tr>
-                <tr>
-                    <td className='form-text' style={{width: '15%'}}><label>Email: </label></td>
-                    <td style={{width: '85%'}}><input type="email" name="user_email" className='form-input border' placeholder='Email'/></td>
-                </tr>
-                <tr>
-                    <td className='form-text' style={{width: '15%', verticalAlign: 'top'}}><label>Message: </label></td>
-                    <td colSpan='3' style={{width: '85%', height: '160px'}}><textarea name="message" className='form-input border' placeholder='Message' style={{height: '155px'}}/></td>
-                </tr>
-                <tr>
-                    <td colSpan='4' style={{textAlign: 'center'}}><input type="submit" value="Send" className='form-button button'/></td>
-                </tr>
-            </table>
-        </form>
+                <table className={`form-table ${clicked ? '' : 'hidden'}`}>
+                        <tr className={`${clicked ? '' : 'hidden'}`}>
+                            <td  className={`form-text ${clicked ? '' : 'hidden'}`}><label>Name: </label></td>
+                            <td style={{width: '85%'}} ><input type="text" name="from_name" className={`form-input ${clicked ? '' : 'display-none'}`} placeholder='Name' required/></td>
+                        </tr>
+                        <tr className={`${clicked ? '' : 'hidden'}`}>
+                            <td className='form-text' ><label>Email: </label></td>
+                            <td style={{width: '85%'}}><input type="email" name="user_email" className={`form-input ${clicked ? '' : 'display-none'}`} placeholder='Email' required/></td>
+                        </tr>
+                        <tr className={`${clicked ? '' : 'hidden'}`}>
+                            <td className='form-text' style={{verticalAlign: 'top'}}><label>Message: </label></td>
+                            <td colSpan='3' style={{width: '85%', height: '160px'}}><textarea name="message" className={`form-input ${clicked ? '' : 'display-none'}`} placeholder='Message' style={{height: '155px'}} required/></td>
+                        </tr>
+                        <tr className={`${clicked ? '' : 'hidden'}`}>
+                            <td colSpan='4' style={{textAlign: 'center'}}><input type="submit" value="Send" className={`form-button button ${clicked ? '' : 'display-none'}`} /></td>
+                        </tr>
+                    </table>
+                {/* {formRender} */}
+            </form>
         </div>
     )
 }
